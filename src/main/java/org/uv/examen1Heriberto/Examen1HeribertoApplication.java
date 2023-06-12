c07948a4a30dc16274a28bfe415365a1859abcf4
btoarriola <betokanon@gmail.com>
2023-06-12 09:38:33 -0500

    New

---
+++ src/main/java/org/uv/examen1Heriberto/Examen1HeribertoApplication.java
@@ -0,0 +1,13 @@
+package org.uv.examen1Heriberto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Examen1HeribertoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Examen1HeribertoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uv/examen1Heriberto/Examen1HeribertoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uv.examen1Heriberto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Examen1HeribertoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
